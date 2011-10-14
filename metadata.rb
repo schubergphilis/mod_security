647b4fbcdec11a8f0cefd439f03470da3349e693
Jason Rohwedder <jro@honeyapps.com>
2011-10-14 12:04:30 -0500

    add hopefully supported OSes to metadata and repackage

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,13 @@ maintainer_email  "jro@honeyapps.com"
 license           "Apache 2.0"
 description       "Installs mod_security for Apache2"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.3"
+version           "0.0.4"
 
 depends "apache2"
 depends "build-essential"
 
 #%w{ redhat centos ubuntu debian }.each do |os|
-%w{ ubuntu debian }.each do |os|
+%w{ redhat centos scientific fedora ubuntu debian }.each do |os|
   supports os
 end
 
