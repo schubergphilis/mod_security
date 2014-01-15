2e8bc69d45d889372134d6360cc1fe75c5794f79
Gavin Reynolds <g.reynolds@src.gla.ac.uk>
2014-01-15 17:04:04 +0000

    Consistently naming the tests and added more in-depth tests. (+1 squashed commit) Squashed commits: [4d6afca] Using serverspec busser for server testing. Basic Debian platform testing for installed apache and configured mod_security

---
+++ test/integration/default/serverspec/mod_security_spec.rb
@@ -0,0 +1,40 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.path = '/sbin:/usr/sbin'
+  end
+end
+
+describe "Apache2 with mod_security" do
+
+  it "apache2 should be installed" do
+    expect(package("apache2")).to be_installed
+  end
+
+  it "apache2 should be running" do
+    expect(service("apache2")).to be_running
+  end
+
+  it "apache2 is listening on port 80" do
+    expect(port(80)).to be_listening
+  end
+
+  it "apache2 has a mod-security configuration file" do
+    expect(file("/etc/apache2/mods-enabled/mod-security.conf")).to be_file
+    expect(file("/etc/apache2/mods-enabled/mod-security.conf")).to contain 'Include "/etc/apache2/mod_security/*.conf"'
+  end
+
+  it "apache2 has a mod-security load file" do
+    expect(file("/etc/apache2/mods-enabled/mod-security.load")).to be_file
+  end
+
+  it "mod_security is configured" do
+    expect(file("/etc/apache2/mod_security/modsecurity.conf")).to be_file
+    expect(file("/etc/apache2/mod_security/modsecurity.conf")).to contain 'SecRuleEngine DetectionOnly'
+  end
+
+end
