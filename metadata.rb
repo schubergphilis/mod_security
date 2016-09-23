2c73b5bd2ea83f99a43076e405a357751ed66626
Frank Breedijk <fbreedijk@schubergphilis.com>
2016-09-23 11:53:07 +0200

    Need to bump version too

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'jro@honeyapps.com'
 license           'Apache 2.0'
 description       'Installs and configures mod_security for Apache2 or IIS'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.2.3'
+version           '0.2.4'
 
 depends 'apache2'
 depends 'build-essential'
