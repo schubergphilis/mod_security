055a3cbac6627dc0ab8fe15b19234af0d3ab5373
Frank Breedijk <fbreedijk@schubergphilis.com>
2017-01-30 16:06:40 +0100

    Fixes #44 - @SharEdgeMarshall pointed out that IncludeOption is better

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name              'mod_security'
-maintainer        'HoneyApps Inc.'
-maintainer_email  'jro@honeyapps.com'
+maintainer        'Schuberg Philis'
+maintainer_email  'fbreedijk@schubergphilis.com'
 license           'Apache 2.0'
 description       'Installs and configures mod_security for Apache2 or IIS'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.2.5'
+version           '0.2.6'
 
 depends 'apache2'
 depends 'build-essential'
